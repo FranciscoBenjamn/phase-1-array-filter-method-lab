fbdd60eb63d18f3a8258431173d51beafaeffb96
Franklin Benjamin <franciscovibes14@gmail.com>
2022-06-10 14:55:25 -0400

    commit tree 58708bc46862bfc1aa4d103a2f5b94d6b6cef4d8

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase().includes(name.toLowerCase()));
+}
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase().includes(name.toLowerCase()));
+}
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name.toLowerCase().includes(name.toLowerCase()));
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(driver => {
+  
+
+    if (driver.charAt(2) === name && driver.charAt(driver.length - 1) === name){
+        return;
+    }
+
+    return  (driver.substring(0,2) === name.substring(0,2))
+    })
+
+}
